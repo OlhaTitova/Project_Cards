bfcc66ce3392a5b2702453091e54c13bd4dd2e00
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2020-12-14 01:23:47 +0200

    add some js

--- js/forms.js
+++ js/forms.js
@@ -1,7 +1,7 @@
 class TextArea {
-    constructor(rows = 1) {
+    constructor(rows = 1, classList) {
         this.area = document.createElement('textarea')
-        this.area.classList.add('form-control')
+        this.area.classList = classList
         this.area.rows = rows
         return this.area
     }
@@ -68,31 +68,45 @@ class CreateVisitForm {
         this.createButton = new Input('submit', 'btn btn-success', 'Create')
         this.closeButton = new Input('button', 'btn btn-secondary btn-sm', 'Close')
         this.doctors = new Select('Choose a Doctor', 'Cardiologist', 'Dentist', 'Therapist')
+        this.doctors.classList.add('doctors-list')
 
-        this.visitGoal = new Input('text', 'form-control', null, 'Purpose of the visit')
-        this.age = new Input('number', 'form-control', null, 'Enter your age')
-        this.name = new Input('text', 'form-control', null, 'Name, Surename')
-        this.visitComment = new TextArea()
+        this.visitGoal = new Input('text', 'form-control common pop-up', null, 'Purpose of the visit')
+        this.age = new Input('number', 'form-control common pop-up', null, 'Enter your age')
+        this.name = new Input('text', 'form-control common pop-up', null, 'Name, Surename')
+
+        this.visitComment = new TextArea(1, 'form-control common pop-up')
         this.visitComment.placeholder = 'Short comment'
+
         this.priority = new Select('Priority', 'Low', 'Normal', 'High')
-        this.commonFields = [this.name, this.visitGoal, this.age, this.visitComment, this.priority]
+        this.priority.classList += ' common pop-up'
 
-        this.pressure = new Input('text', 'form-control', null, 'Normal pressure')
-        this.bodyWeight = new Input('text', 'form-control', null, 'body weight')
-        this.heartDiseases = new Input('text', 'form-control', null, 'heart diseases')
-        this.cardiologistFields = [this.pressure, this.bodyWeight, this.heartDiseases]
-        this.lastVisit = new Input('text', 'form-control', null, 'dd.mm.yy')
+        this.pressure = new Input('text', 'form-control cardiologist pop-up', null, 'Normal pressure')
+        this.bodyWeight = new Input('text', 'form-control cardiologist pop-up', null, 'body weight')
+        this.heartDiseases = new Input('text', 'form-control cardiologist pop-up', null, 'heart diseases')
 
-        this.form.append(this.doctors)
-        for (let field of this.commonFields) {
+        this.lastVisit = new Input('text', 'form-control dentist pop-up', null, 'dd.mm.yy')
+
+        this.fields = [
+            this.doctors,
+            this.name,
+            this.visitGoal,
+            this.age,
+            this.priority,
+            this.pressure,
+            this.bodyWeight,
+            this.heartDiseases,
+            this.lastVisit,
+            this.visitComment,
+            this.createButton,
+            this.closeButton,
+        ]
+
+        for (let field of this.fields) {
             this.form.appendChild(field)
-            // field.hidden = true
+            if (field.classList.contains('pop-up')) {
+                field.hidden = true
+            }
         }
-        for (let field of this.cardiologistFields) {
-            this.form.appendChild(field)
-        }
-        this.form.append(this.lastVisit)
-        this.form.append(this.createButton, this.closeButton)
         return this.form
 
         // кардиолог: обычное давление, вес тела,заболевания ссс
@@ -103,3 +117,19 @@ class CreateVisitForm {
 const modal = document.querySelector('.modal-ne-bootstrap')
 const test = new CreateVisitForm()
 modal.appendChild(test)
+
+const doctors = document.querySelector('.doctors-list')
+doctors.addEventListener('change', showFields)
+function showFields(e) {
+    const commonFields = document.querySelectorAll('.pop-up.common')
+    for (field of commonFields) {
+        field.hidden = false
+    }
+    const selected = doctors.value
+    if (selected === 'Cardiologist') {
+        const cardiologistFields = document.querySelectorAll('.pop-up.cardiologist')
+        for (let field of cardiologistFields) {
+            field.hidden = false
+        }
+    }
+}
